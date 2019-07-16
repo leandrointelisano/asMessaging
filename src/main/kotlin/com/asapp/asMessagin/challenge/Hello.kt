f42b461b2e6a4b10d7f1d3bbca5979aaf291c4ed
Leandro Intelisano <leandro.intelisano@despegar.com>
2019-07-16 00:33:27 -0300

    [MSG-001] Initial mvn project structure

---
+++ src/main/kotlin/com/asapp/asMessagin/challenge/Hello.kt
@@ -0,0 +1,6 @@
+package com.asapp.asMessagin.challenge
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/asapp/asMessagin/challenge/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.asapp.asMessagin.challenge
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
